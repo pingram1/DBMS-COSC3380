656881208af947784da9fa4c92ab7be24a66f53c
Long Thanh Tran <longttran98@gmail.com>
2024-10-16 19:26:03 -0500

    remove key

--- backend/app.py
+++ backend/app.py
@@ -3,8 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://avnadmin:AVNS_4iwhblDyCsltSmeJoj8@mysql-posdatabase-database-pos-3380.l.aivencloud.com:27790/defaultdb?ssl-mode=REQUIRED'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://127.0.0.1:3306/?user=root'
+# app.config['SQLALCHEMY_DATABASE_URI'] = ''
+app.config['SQLALCHEMY_DATABASE_URI'] = ''
 
 db = SQLAlchemy(app)
 
